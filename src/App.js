df3a07055e7a7c36d45037afeb61d3af68ea5116
unknown <matheus.mhg2@gmail.com>
2023-02-23 22:36:24 -0300

    feature: adding axios connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Grid from "./components/Grid";
+import Navbar from "./components/Navbar";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const [photos, setPhotos] = useState([]);
+  const [updateUI, setUpdateUI] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/get")
+      .then((res) => {
+        console.log(res.data);
+        setPhotos(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [updateUI]);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Grid photos={photos} />
+      <Button setUpdateUI={setUpdateUI} />
+    </div>
+  );
 }
 
 export default App;
